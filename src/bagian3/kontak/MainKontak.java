c5cffc9c32607c1415ac5b8a7597ee1455a6accf
2410010054elba <2410010054elba@gmail.com>
2026-06-26 16:22:28 +0800

    Selesai Bagian 3: Multi-Class

---
+++ src/bagian3/kontak/MainKontak.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bagian3.kontak;
+
+public class MainKontak {
+    public static void main(String[] args) {
+        BukuKontak buku = new BukuKontak("kontak.txt");
+        buku.tambahKontak(new Kontak("Andi", "0811111", "andi@mail.com"));
+        buku.tambahKontak(new Kontak("Budi", "0822222", "budi@mail.com"));
+        buku.tambahKontak(new Kontak("Citra", "0833333", "citra@mail.com"));
+        buku.tampilkanSemua();
+        buku.simpanKeBerkas();
+
+        System.out.println();
+
+        buku.cariKontak("Budi");
+        buku.cariKontak("Eka");
+
+        System.out.println();
+
+        buku.hapusKontak("Andi");
+        buku.tampilkanSemua();
+
+        System.out.println();
+
+        BukuKontak bukuLain = new BukuKontak("kontak.txt");
+        bukuLain.muatDariBerkas();
+        bukuLain.tampilkanSemua();
+        System.out.println("Jumlah kontak: " + bukuLain.jumlahKontak());
+    }
+}
